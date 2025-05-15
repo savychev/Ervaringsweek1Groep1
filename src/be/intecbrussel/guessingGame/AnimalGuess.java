6d8937eec5f4ab1d0e83a3cf34964e4df2ad89b9
savychev <werk3093059@gmail.com>
2025-05-15 14:55:59 +0200

    GuessingGame

---
+++ src/be/intecbrussel/guessingGame/AnimalGuess.java
@@ -0,0 +1,38 @@
+package be.intecbrussel.guessingGame;
+
+import java.util.Random;
+
+// Dier-gokuitdaging
+class AnimalGuess implements Guessable {
+    private static final String[] ANIMALS = {"hond", "kat", "olifant", "leeuw", "tijger"}; // Mogelijke dieren om uit te kiezen
+    private final String secret; // Het geheime dier
+    private boolean guessed; // Geeft aan of het dier al geraden is
+
+    /**
+     * Constructor: kiest willekeurig een dier en zet de status op niet geraden.
+     */
+    public AnimalGuess() {
+        this.secret = ANIMALS[new Random().nextInt(ANIMALS.length)];
+        this.guessed = false;
+    }
+
+    @Override
+    public boolean isGuessed() {
+        return guessed;
+    }
+
+    @Override
+    public void prompt() {
+        System.out.print("Raad het dier: ");
+    }
+
+    @Override
+    public void guess(String input) {
+        if (input.equals(secret)) {
+            System.out.println("Dat klopt! Het dier is een " + secret + ".");
+            guessed = true;
+        } else {
+            System.out.println("Helaas, dat is niet het juiste dier.");
+        }
+    }
+}
---
+++ src/be/intecbrussel/guessingGame/MainApp.java
@@ -0,0 +1,7 @@
+package be.intecbrussel.guessingGame;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new GuessingGame().start();
+    }
+}
